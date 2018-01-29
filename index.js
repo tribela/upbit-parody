4b5c6eb84255994a0074bbfcca64ab423279d59f
kjwon15 <kjwonmail@gmail.com>
2018-01-29 15:50:04 +0900

    Edit main text

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 import './master.scss';
+
+window.addEventListener('load', () => {
+    document.body.addEventListener('click', (event) => {
+        let target = event.target;
+        while (target.nodeName !== 'A') {
+            target = target.parentElement;
+            if (target === document.body) {
+                return;
+            }
+        }
+
+        if (target.host !== location.host) {
+            event.preventDefault();
+            window.open(target.href);
+        }
+    })
+})
