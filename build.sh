1b46fbf5e3c957f3b855a54518ccc9ec21f6055d
kjwon15 <kjwonmail@gmail.com>
2018-01-30 11:06:37 +0900

    Change build script for netlify

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 
 set -eo pipefail
 
-git checkout site
-git reset --hard master
 yarn build
 mkdir -p dist/imgs
 cp src/imgs/cover.png dist/imgs/cover.png
-git add -f dist
-git commit -m 'Build'
-git checkout master
